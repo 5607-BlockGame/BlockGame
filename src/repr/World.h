c91b2eb5b0273d228ddba40261f58fd7b2acd98a
Andrew Gazelka <andrew.gazelka@gmail.com>
2021-04-21 20:02:55 -0500

    remove unused classes from hw04

--- src/repr/World.h
+++ src/repr/World.h
@@ -8,63 +8,6 @@
 #include "Chunk.h"
 #include "Generator.h"
 
-enum class Tag {
-    START = 0,
-    FINISH,
-    DOOR,
-    KEY,
-    WALL,
-    EMPTY
-};
-
-
-struct Door {
-    size_t id;
-
-    bool operator==(const Door &rhs) const;
-
-    bool operator!=(const Door &rhs) const;
-};
-
-struct Key {
-    size_t id;
-
-    bool operator==(const Key &rhs) const;
-
-    bool operator!=(const Key &rhs) const;
-};
-
-struct Empty {
-    static Empty Get() {
-        return {};
-    }
-};
-
-
-struct Element {
-    Tag tag;
-    union Value {
-        Empty empty;
-        Door door;
-        Key key;
-    } value;
-
-    static Element Start();
-
-    static Element Finish();
-
-    static Element Empty();
-
-    static Element Wall();
-
-    static Element Door(size_t id);
-
-    static Element Key(size_t id);
-
-    bool operator==(const Element &rhs) const;
-
-    bool operator!=(const Element &rhs) const;
-};
 
 struct PlayerData {
     Vec3D<double> location;

d047391e51eccd74e9ccc93ec2bdc97e12579e89
Andrew Gazelka <andrew.gazelka@gmail.com>
2021-04-30 14:37:41 -0500

    add simple simplex noise

--- src/generator/SimpleGenerator.cpp
+++ src/generator/SimpleGenerator.cpp
@@ -2,15 +2,32 @@
 // Created by Andrew Gazelka on 4/21/21.
 //
 
+#include <SimplexNoise/src/SimplexNoise.h>
 #include "SimpleGenerator.h"
 
 Chunk SimpleGenerator::Generate(ChunkCoord coord) {
 
     Chunk chunk;
 
-    for (size_t z = 0; z < CHUNK_HEIGHT; ++z) {
-        for (size_t x = 0; x < CHUNK_WIDTH; ++x) {
-            for (size_t y = 0; y < CHUNK_WIDTH; ++y) {
+    int startX = coord.x << 4;
+    int startY = coord.y << 4;
+
+    for (size_t x = 0; x < CHUNK_WIDTH; ++x) {
+        for (size_t y = 0; y < CHUNK_WIDTH; ++y) {
+
+            float actualX = startX + x;
+            float actualY = startY + y;
+
+            float sx = actualX * 0.1f;
+            float sy = actualY * 0.1f;
+
+
+            // [0, 1]
+            float noiseVal = (SimplexNoise::noise(sx, sy) + 1.0f) / 2.0f;
+
+            int height = (int)(CHUNK_HEIGHT * noiseVal);
+
+            for (size_t z = 0; z < height; ++z) {
 
                 Vec3D<size_t> location(x, y, z);
 
